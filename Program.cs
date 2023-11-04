1e5bda59865991814b5fafd8829d91382413d9e9
Wiktor Kycia <wiktor.kycia@gmail.com>
2023-11-04 17:26:46 +0100

    add beginning information and question about number of measurements

--- Program.cs
+++ Program.cs
@@ -3,6 +3,7 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Console.WriteLine("We will be calculating the area of a page of paper");
+        Console.WriteLine("How many measurements are we going to do?");
     }
 }
