52643ddd115ce374c6f1f1f2c088583107844136
Wiktor Kycia <wiktor.kycia@gmail.com>
2023-11-04 17:42:11 +0100

    make a do while loop that takes user input till it's correct

--- Program.cs
+++ Program.cs
@@ -4,6 +4,11 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("We will be calculating the area of a page of paper");
-        Console.WriteLine("How many measurements are we going to do?");
+        int amount;
+        do
+        {
+            Console.Clear();
+            Console.WriteLine("How many measurements are we going to do?");
+        } while (!int.TryParse(Console.ReadLine(), out amount));
     }
 }
