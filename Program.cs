0b446209e535336fb9479af4116d07aa9211dfa2
Wiktor Kycia <wiktor.kycia@gmail.com>
2023-11-04 17:47:17 +0100

    add an array of measurements to which we will write down scores of measurements

--- Program.cs
+++ Program.cs
@@ -10,5 +10,7 @@ class Program
             Console.Clear();
             Console.WriteLine("How many measurements are we going to do?");
         } while (!int.TryParse(Console.ReadLine(), out amount));
+
+        double[] measurements = new double[amount];
     }
 }
